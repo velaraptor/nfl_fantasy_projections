b63f30596497be6519db127f3555e45b37d89241
chris vela <info@christophvel.com>
2019-08-23 17:45:08 -0500

    add monte carlo script

---
+++ monte_carlo_main.py
@@ -0,0 +1,47 @@
+from nfl_draft import DraftState, NflPlayer, GetMoves, GetResult, DoMove, Clone
+from monte_carlo import UCT
+import pandas as pd
+import numpy as np
+
+
+def main():
+    nfl_players = pd.read_csv('all_predictions.csv')
+    nfl_players['mean_prediction'] = nfl_players.apply(lambda x: np.mean(x[['low', 'point', 'prediction']]), axis=1)
+    nfl_players = nfl_players[['Name', 'FantPos', 'mean_prediction', 'high', 'low']]
+    nfl_players = nfl_players[nfl_players.mean_prediction > 75].reset_index(drop=True)
+    nfl_players = nfl_players.sort_values('mean_prediction', ascending=False)
+    freeagents = [NflPlayer(*p) for p in nfl_players.itertuples(index=False, name=None)]
+    num_competitors = 12
+    rosters = [[] for _ in range(num_competitors)]  # empty rosters to start with
+
+    num_rounds = 15
+    turns = []
+    # generate turns by snake order
+    for i in range(num_rounds):
+        turns += reversed(range(num_competitors)) if i % 2 else range(num_competitors)
+
+    DraftState.GetResult = GetResult
+    DraftState.GetMoves = GetMoves
+    DraftState.DoMove = DoMove
+    DraftState.Clone = Clone
+
+    state = DraftState(rosters, turns, freeagents)
+    iterations = 100000
+    while state.GetMoves() != []:
+        move = UCT(state, iterations)
+        state.DoMove(move)
+
+    final_rosters = pd.DataFrame({"Team " + str(i + 1): r for i, r in enumerate(state.rosters)})
+    print(final_rosters.head())
+    print('Total Points')
+    for i in np.arange(0, len(state.rosters)):
+        one_team = pd.Series(state.rosters[i])
+        t = one_team.astype(str).str.split('|', expand=True)
+        print({i + 1: np.sum(t[2].astype(float))})
+
+    final_rosters.to_csv('final_rosters.csv')
+    return final_rosters
+
+
+if __name__ == '__main__':
+    main()
---
+++ nfl_draft.py
@@ -0,0 +1,91 @@
+import numpy as np
+
+
+class DraftState:
+    def __init__(self, rosters, turns, freeagents, playerjm=None):
+        self.rosters = rosters
+        self.turns = turns
+        self.freeagents = freeagents
+        self.playerJustMoved = playerjm
+
+
+class NflPlayer:
+    def __init__(self, name, position, points, high, low):
+        self.name = name
+        self.position = position
+        self.points = points
+        self.high = high
+        self.low = low
+
+    def __repr__(self):
+        return "|".join([self.name, self.position, str(self.points)])
+
+
+def GetResult(self, playerjm):
+    """ Get the game result from the viewpoint of playerjm.
+    """
+    if playerjm is None: return 0
+
+    pos_wgts = {
+        ("QB"): [.5],
+        ("WR"): [.5, .5, .5, .5],
+        ("RB"): [.5, .5, .5, .5],
+        ("TE"): [.5, .5],
+        ("RB", "WR", "TE"): [.5, .5, .5]
+    }
+
+    result = 0
+    # map the drafted players to the weights
+    for p in self.rosters[playerjm]:
+        max_wgt, _, max_pos, old_wgts = max(
+            ((wgts[0], -len(lineup_pos), lineup_pos, wgts) for lineup_pos, wgts in pos_wgts.items()
+             if p.position in lineup_pos),
+            default=(0, 0, (), []))
+        if max_wgt > 0:
+
+            random_points = np.random.randint(p.low, p.high + 1, 1)[0]
+            result += max_wgt * random_points
+            old_wgts.pop(0)
+            if not old_wgts:
+                pos_wgts.pop(max_pos)
+
+    # map the remaining weights to the top three free agents
+    for pos, wgts in pos_wgts.items():
+        result += np.mean(
+            [np.random.randint(p.low, p.high + 1, 1)[0] for p in self.freeagents if p.position in pos][:5])
+    return result
+
+
+def GetMoves(self):
+    """ Get all possible moves from this state.
+    """
+    pos_max = {"QB": 1, "WR": 6, "RB": 6, "TE": 2}
+
+    if len(self.turns) == 0: return []
+
+    roster_positions = np.array([p.position for p in self.rosters[self.turns[0]]], dtype=str)
+    moves = [pos for pos, max_ in pos_max.items() if np.sum(roster_positions == pos) < max_]
+    return moves
+
+
+def DoMove(self, move):
+    """ Update a state by carrying out the given move.
+        Must update playerJustMoved.
+    """
+    player = next(p for p in self.freeagents if p.position == move)
+    self.freeagents.remove(player)
+    rosterId = self.turns.pop(0)
+    self.rosters[rosterId].append(player)
+    self.playerJustMoved = rosterId
+
+
+DraftState.DoMove = DoMove
+
+
+def Clone(self):
+    """ Create a deep clone of this game state.
+    """
+    rosters = list(map(lambda r: r[:], self.rosters))
+    st = DraftState(rosters, self.turns[:], self.freeagents[:],
+                    self.playerJustMoved)
+    return st
